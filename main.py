5986a57a052ad942e76383383e1f7f717ade436b
Usman Sagir Usman <walidsagir8@gmail.com>
2026-08-28 08:59:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print("Starting FastAPI backend server application...")
 
 @app.get("/")
 def read_root():
-    with open("/template/index.html", "r") as f:
+    with open("template/index.html", "r") as f:
         html_content = f.read()
     return HTMLResponse(content=html_content, media_type="text/html")
 
